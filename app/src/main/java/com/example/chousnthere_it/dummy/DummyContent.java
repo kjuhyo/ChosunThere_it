4bc3da8240b670312b4a2c39f2ab4a5c2cd0fa25
yoonsoyoung <32251962+yoonsoyoung@users.noreply.github.com>
2019-11-24 03:06:41 +0900

    Univ List 의 대학건물 리스트 출력(DummyContent.java 수정)

--- app/src/main/java/com/example/chousnthere_it/dummy/DummyContent.java
+++ app/src/main/java/com/example/chousnthere_it/dummy/DummyContent.java
@@ -1,5 +1,12 @@
 package com.example.chousnthere_it.dummy;
 
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.chousnthere_it.R;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,7 +18,9 @@ import java.util.Map;
  * <p>
  * TODO: Replace all uses of this class before publishing your app.
  */
-public class DummyContent {
+public class DummyContent extends AppCompatActivity {
+    private static int buildingNum = 33; //대학건물 수
+
 
     /**
      * An array of sample (dummy) items.
@@ -23,10 +32,11 @@ public class DummyContent {
      */
     public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
 
-    private static final int COUNT = 25;
+    private static final int COUNT = buildingNum;
 
     static {
         // Add some sample items.
+
         for (int i = 1; i <= COUNT; i++) {
             addItem(createDummyItem(i));
         }
@@ -38,7 +48,7 @@ public class DummyContent {
     }
 
     private static DummyItem createDummyItem(int position) {
-        return new DummyItem(String.valueOf(position), "Item " + position, makeDetails(position));
+        return new DummyItem(String.valueOf(position),buildingArr(position) , makeDetails(position));
     }
 
     private static String makeDetails(int position) {
@@ -49,7 +59,26 @@ public class DummyContent {
         }
         return builder.toString();
     }
+    private static String buildingArr(int position) {
+        String output = null;
+        String[] buildingArr2 = new String[]{
+                "IT 융합대학","인문과학대학","자연과학대학","법과대학","사회과학대학","경상대학",
+                "공과대학 제 1공학관","공과대학 제 2공학관","사범대학","외국어대학","체육대학",
+                "의과대학","치과대학","약학대학","미술대학","기초교육대학","보건과학대학",
+                "미래사회융합대학","중앙도서관","해오름관", "입석홀","글로벌하우스","서석홀",
+                "백학학사","본관","솔마루","황금추관","학생회관","장황남 정보통신박물관","국제관",
+                "법학도서관","선박해양공학관","항공우주공학관" };
+       for(int i=0; i<buildingArr2.length-1; i++) {
+           if(i==position) {
+               output = buildingArr2[position];
+           }
+       }
+        StringBuilder builder = new StringBuilder();
+        builder.append(output);
 
+
+        return builder.toString();
+    }
     /**
      * A dummy item representing a piece of content.
      */
